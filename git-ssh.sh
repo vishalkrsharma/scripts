d8ee165cd041db4b05dcf1cf3e12802dc47f0a57
vishalkrsharma <vishalkrsharma1234@gmail.com>
2022-01-22 18:39:46 +0530

    git-ssh.sh

---
+++ git-ssh.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/zsh
+
+# Generate SSH Key
+
+ssh-keygen -t ed25519 -C "vishalkrsharma1234@gmail.com" -yy
+
+# Start SSH Agent
+
+eval "$(ssh-agent -s)"
+
+cat ~/.ssh/id_ed25519.pub
--- post-Install.sh
+++ post-Install.sh
@@ -3,9 +3,9 @@
 # Basic Utilities
 
 cd ~
-sudo apt install update -qq
-sudo apt install upgrade -qq
-sudo apt install -yy htop gdebi preload synaptic obs-studio chrome-gnome-shell dconf-editor cmatrix git vlc virt-manager cava ranger gdebi-core wget
+sudo apt install update
+sudo apt install upgrade
+sudo apt install -yy htop gdebi preload synaptic obs-studio chrome-gnome-shell dconf-editor cmatrix git vlc virt-manager cava ranger gdebi-core wget vim curl zsh steam
 
 # Codecs and MS Fonts
 
@@ -18,7 +18,7 @@ git config --global user.email "vishalkrsharma1234@gmail.com"
 
 # Ubuntu Customizers
 
-sudo apt install -yy chrome-gnome-shell dconf-editor gnome-tweak-tool gnome-shell-extensions
+sudo apt install -yy chrome-gnome-shell dconf-editor gnome-tweaks gnome-shell-extensions
 
 # Fonts
 
@@ -31,17 +31,20 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt install ./google-chrome-stable_current_amd64.deb
 cd ~/
 
+# Firefox
+
+sudo snap remove firefox
+sudo apt install -yy firefox
+
 # Visual Studio Code
 
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
 sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
 rm -f packages.microsoft.gpg
-
-# Firefox
-
-sudo snap remove firefox
-sudo apt install -yy firefox
+sudo apt install apt-transport-https
+sudo apt update
+sudo apt install code
 
 # Regolith Linux
 
@@ -60,12 +63,34 @@ sudo apt install --y python3 pip3
 
 cd Downloads/
 wget -O ~/discord.deb "https://discordapp.com/api/download?platform=linux&format=deb"
-sudo gdebi ~/discord.deb
+sudo gdebi ~/discord.deb -y
 cd ~/
 
-# Steam
+# NodeJS
 
-sudo apt install -yy steam
+curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+# Wonderwall
+
+sudo snap install wonderwall
+
+# Zsh Default Shell
+
+chsh -s $(which zsh)
+
+# Zsh Plugins
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# OhMyZsh
+
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Powerline10k
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 # End
 
